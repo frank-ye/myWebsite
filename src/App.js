4a5d1c2263e4fbdc252435a9dced9a875ef1d9fc
frank-ye <frank.jing.ye@gmail.com>
2019-08-17 18:12:41 -0400

    Created new app repository with nested components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Ninjas from './Ninjas'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React app!</h1>
+      <p>Welcome to the shadow realm</p>
+      <Ninjas />
+    </div>
+  );
+}
+
+export default App;
